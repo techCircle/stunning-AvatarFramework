f5198f0d3154ad37a28faa369fe6e56675625062
TechCircle,LLC <53327631+techCircle@users.noreply.github.com>
2024-07-01 21:03:39 -0400

    api verification

---
+++ src/test/java/RestAPI_Testing/RestAssured_listAndVerifyBoardInformation.java
@@ -0,0 +1,24 @@
+package RestAPI_Testing;
+
+import Utilities.BaseClass;
+import io.restassured.RestAssured;
+import static org.hamcrest.Matchers.*;
+
+public class RestAssured_listAndVerifyBoardInformation {
+
+	public static void main(String[] args) {
+		
+		
+		RestAssured.given()
+		.param("key", BaseClass.getProperty("key"))
+		.param("token", BaseClass.getProperty("token"))
+		.get(BaseClass.getProperty("baseURL"))
+		.then().statusCode(200)   //checks for status code.. if it fails, it will break and stop
+		.body("id[0]", equalTo("667a18de4ffb6fda6a592e47"))
+		.body("name", hasItems("Ali Family","El-Khatib Family")); //if it fails to verify, it will break
+	
+		
+		
+	}
+
+}
